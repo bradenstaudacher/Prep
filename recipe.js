756f899ac162100129211de8bc2d6178b38ca7c4
bradenstaudacher <braden.staudacher@gmail.com>
2015-02-24 00:25:03 -0800

    Added Comments
    Added comments to clear up functionality

--- recipe.js
+++ recipe.js
@@ -2,43 +2,45 @@ $(document).ready(function() {
 	$(".convert").click(function() {
 		if ($(this).text() === 'Imperial') {
         	$(this).text('Metric');
+        	// add functionality here
     	}
     	else {
         	$(this).text('Imperial');
-     
+        	// add funcionality here
+
     	}
 	});
 	
-	$(".vegetarian").click(function() {
+	$(".vegetarian").click(function() { 
 		if ($(this).text() === 'Vegetarian') {
-        	$(this).text('Add Meat');
-        	$(".nveg").toggle();
+        	$(this).text('Add Meat'); //changes text to say Add Meat
+        	$(".nveg").toggle(); //hides or shows the items on the list that match the class nveg
     	}
     	else {
-        	$(this).text('Vegetarian');
+        	$(this).text('Vegetarian'); //changes back the text to Vegetarian
         	$(".nveg").toggle();
     	}
 	});
 	
 	$(".lactose").click(function() {
-		if ($(this).text() === 'Lactose Intolerant') {
-        	$(this).text('Add Dairy');
-        	$(".nlac").toggle();
+		if ($(this).text() === 'Lactose Intolerant') { 
+        	$(this).text('Add Dairy'); //changes the text to say Add Dairy
+        	$(".nlac").toggle(); //hides or shows the items on the list that match the class nlac
     	}
     	else {
-        	$(this).text('Lactose Intolerant');
+        	$(this).text('Lactose Intolerant'); //changes back the text to Lactose Intolerant
         	$(".nlac").toggle();
     	}
 	});
 
-	$("#photooff").click(function() {
+	$("#photooff").click(function() { //hides photos on click of the text
 		if ($(this).text() === 'Hide Photos') {
         	$(this).text('Show Photos');
-        	$("img").toggle();
-        	$(".imgtext").toggle();
+        	$("img").toggle(); //toggles the images off
+        	$(".imgtext").toggle(); //toggles the text overlay of the images off
     	}
     	else {
-        	$(this).text('Hide Photos');
+        	$(this).text('Hide Photos'); //same as above but switches the function and text
         	$("img").toggle();
         	$(".imgtext").toggle();
     	}
