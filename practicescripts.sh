2034cbc3b9920fbeb9c5bf14c4b8a67aba198e2a
bradenstaudacher <braden.staudacher@gmail.com>
2015-03-02 18:33:46 -0800

    Scripts
    Scripts to complete exercises

---
+++ practicescripts.sh
@@ -0,0 +1,5 @@
+history | grep "cat" | wc -l
+cat /usr/share/dict/words | grep "^a" | wc -l
+uname -a Linux 3.2.0
+cat /usr/share/dict/words | grep "^a.*s$"
+wget "www.lighthouselabs.ca/index.html" -O lightouse.txt
